f2d8678a3090d2a51e99a18067814f38a8fae7af
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-21 02:30:52 -0500

    Se agregaron configuraciones en json y el paquete nodemon

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+//Settings
+
+
+
+//Middlewares  **Se ejecutan antes de ejecutar las rutas
+
+
+//Routes
+
+
+
+//Static files   *** Aqui es donde van los archivos como imagenes o html,js, css,
+
+
+//Starting the server
+app.listen(3000,()=>{
+    console.log('Server on Port 3000');
+});
