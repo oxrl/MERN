07f5488ba3bb7b3640dc8d81a6430ef9da244ecc
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-22 10:02:24 -0500

    Se crearon las configuraciones del server y un router inicial

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const morgan  = require('morgan');
 const app = express();
 
 
 //Settings
-
+const port  = process.env.PORT || 3000;
 
 
 //Middlewares  **Se ejecutan antes de ejecutar las rutas
-
-
+app.use(morgan('dev'));
+app.use(express.json());
 //Routes
-
+app.use('/api/tasks',require('./routes/task.routes'));
 
 
 //Static files   *** Aqui es donde van los archivos como imagenes o html,js, css,
 
 
 //Starting the server
-app.listen(3000,()=>{
-    console.log('Server on Port 3000');
+app.listen(port,()=>{
+    console.log(`Server on Port ${port}`);
 });
