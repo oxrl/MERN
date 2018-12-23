d82441ffb6d924f85e2b85b841bdc8477960a259
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-23 02:35:15 -0500

    Se termino el backend del proyecto que esta basado en el Tutorial de Fast

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan  = require('morgan');
+const path = require('path');
+
+const { mongoose } = require('./database');
+
 const app = express();
 
 
@@ -15,7 +19,7 @@ app.use('/api/tasks',require('./routes/task.routes'));
 
 
 //Static files   *** Aqui es donde van los archivos como imagenes o html,js, css,
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Starting the server
 app.listen(port,()=>{
